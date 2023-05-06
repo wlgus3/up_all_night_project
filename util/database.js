44445bca321149b10b77293e1fcced7a05cc452e
Jihyun <jhjh3903@gmail.com>
2023-05-07 00:43:33 +0900

    feat: Next.js 환경, MongoDB DB 연결완료

---
+++ util/database.js
@@ -0,0 +1,23 @@
+// const client = await MongoClient.connect("mongodb+srv://pullhorizon:admin1234@cluster0.u8rrkhh.mongodb.net/?retryWrites=true&w=majority", {
+//     useNewUrlParser: true,
+//   });
+//   const db = client.db("uppernight");
+//   db.collection("community").find;
+
+// export(client)
+
+//!아래는 몽고디비 개발자들이 추천하는 세팅코드
+import { MongoClient } from "mongodb";
+const url = process.env.MONGODB_URI;
+const options = { useNewUrlParser: true };
+let connectDB;
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url, options).connect();
+  }
+  connectDB = global._mongo;
+} else {
+  connectDB = new MongoClient(url, options).connect();
+}
+export { connectDB };
