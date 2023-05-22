f9cef34f28b6d8390a88ac43b4d47cc044e401ae
Jihyun <jhjh3903@gmail.com>
2023-05-22 21:20:28 +0900

    feat : 지도 띄우기 완료

--- app/cafemap/page.tsx
+++ app/cafemap/page.tsx
@@ -1,11 +1,16 @@
 import { MongoClient } from "mongodb";
 import { connectDB } from "@/util/database.js";
-
+import Map from "./map";
 export default async function Cafe() {
   // const client = await connectDB;
   // const db = client.db("uppernight");
   // const result = await db.collection("community").find().toArray();
   // console.log(result);
 
-  return <div>카페 지도페이지.</div>;
+  return (
+    <div>
+      <h2>카페 지도페이지 </h2>
+      <Map />
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import { MongoClient } from "mongodb";
 import { connectDB } from "@/util/database.js";
 
 export default async function Home() {
-  const client = await connectDB;
-  const db = client.db("uppernight");
-  const result = await db.collection("community").find().toArray();
-  // console.log(result);
+  // const client = await connectDB;
+  // const db = client.db("uppernight");
+  // const result = await db.collection("community").find().toArray();
 
   return (
-    <div>
-      메인
-      <div> {result[0]["title"]}</div>
-      <div> {result[0]["content"]}</div>
+    <div className="map_page">
+      <h2>메인 페이지</h2>
+      <div>
+        <span>맵ㅁㄴㅇㄹㅁㄹ</span>
+      </div>
     </div>
   );
 }
