f16e79d9eda2e64825d6cc578df48d85dfa87ffb
Jihyun <jhjh3903@gmail.com>
2023-05-23 23:52:27 +0900

    feat: 현위치 마커 표시

--- app/component/map.js
+++ app/component/map.js
@@ -12,6 +12,7 @@ function Map() {
   const [Lng, setLng] = useState();
   const [Lat, setLat] = useState();
   console.log(cafeData[2]);
+  console.log(cafeData.length);
   function location() {
     //!브라우저에서 위치 받아오는 함수
     const { geolocation } = navigator;
@@ -67,20 +68,23 @@ function Map() {
           level: 5, // 지도의 확대 레벨
         };
         const map = new window.kakao.maps.Map(mapContainer, mapOption);
-        //!마커 테스트 성공 -> 광화문 찍어봄
-        // //?마커가 표시 될 위치
-        // let markerPosition = new kakao.maps.LatLng(37.570892, 126.97725);
 
-        // //? 마커를 생성
-        // let marker = new kakao.maps.Marker({
-        //   position: markerPosition,
-        // });
+        //! 내위치 마커 표시
+        var imageSrc = "https://cdn.pixabay.com/photo/2014/04/02/10/45/location-304467_640.png", // 마커이미지의 주소입니다
+          imageSize = new kakao.maps.Size(25, 35), // 마커이미지의 크기입니다
+          imageOption = { offset: new kakao.maps.Point(25, 35) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
-        // //? 마커를 지도 위에 표시
-        // marker.setMap(map);
+        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+          markerPosition = new kakao.maps.LatLng(Lat, Lng); // 마커가 표시될 위치입니다
 
-        //!다중 마커 테스트
-        // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+          position: markerPosition,
+          image: markerImage, // 마커이미지 설정
+        });
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
 
         // var positions = [
         //   //? 데이터 예시 -> 이 형태 맞춰서 넣어주기만 하면 됨
@@ -88,20 +92,8 @@ function Map() {
         //     content: "<div>카카오</div><div>카카오 정보</div><a href='naver.com'>네이버로<a/>",
         //     latlng: new kakao.maps.LatLng(33.450705, 126.570677),
         //   },
-        //   // {
-        //   //   content: "<div>생태연못</div>",
-        //   //   latlng: new kakao.maps.LatLng(33.450936, 126.569477),
-        //   // },
-        //   // {
-        //   //   content: "<div>텃밭</div>",
-        //   //   latlng: new kakao.maps.LatLng(33.450879, 126.56994),
-        //   // },
-        //   // {
-        //   //   content: "<div>근린공원</div>",
-        //   //   latlng: new kakao.maps.LatLng(33.451393, 126.570738),
-        //   // },
         // ];
-
+        //! 다중 마커 표시 -> cafeData만큼 마커정보 만들기
         var positions = [];
         for (let i = 0; i < cafeData.length; i++) {
           positions.push({
@@ -126,7 +118,8 @@ function Map() {
           // 이벤트 리스너로는 클로저를 만들어 등록합니다
           // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
           kakao.maps.event.addListener(marker, "mouseover", makeOverListener(map, marker, infowindow));
-          kakao.maps.event.addListener(marker, "click", makeOutListener(infowindow)); //! 정보창 열고 닫는 이벤트가 약간 이상함 수정요망
+          kakao.maps.event.addListener(marker, "click", makeOverListener(map, marker, infowindow)); //? 좌클릭시 정보표시, 우클릭시 정보창 제거
+          kakao.maps.event.addListener(marker, "rightclick", makeOutListener(infowindow));
         }
 
         // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
