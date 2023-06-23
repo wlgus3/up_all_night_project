00e9ba4bc20ef39fb03de6a7101d24c035763c48
Jihyun <jhjh3903@gmail.com>
2023-06-23 23:22:55 +0900

    feat: 게시글 삭제시 작성자 확인기능

--- app/community/detail/[id]/deletebutton.tsx
+++ app/community/detail/[id]/deletebutton.tsx
@@ -10,10 +10,32 @@ export default function DeleteButton(props) {
       <button
         onClick={() => {
           //단순 스트링이나 숫자가 아닌 객체형식 데이터보낼때 JSON.stringify()안에 담아서 보내면 잘 감
-          // fetch("/api/post/delete", { method: "POST", body: JSON.stringify(props) })
+          //! 방법1) 그냥 delete로 보내기
+          fetch("/api/post/delete", { method: "POST", body: JSON.stringify(props) })
+            .then((res) => {
+              if (res.status == 200) {
+                return res.json(); //? 서버로부터 res로 받은 문구를 직접 alert에 띄우기 위한 코드
+              } else {
+                return res.json();
+                //서버가 500, 400등 에러코드전송시 실행할코드
+              }
+            })
+            .then((res) => {
+              alert(res);
+            })
+            .then(() => {
+              location.href = "/community";
+            })
+            .catch((error) => {
+              //인터넷문제 등으로 실패시 실행할코드
+              console.log(error);
+            });
+
+          //! 방법2) URL parameter로 구현
+          // fetch(`/api/post/delete/${props._id}`, { method: "GET" }) //props 자체가 {_id:xxx..}형태이기에
           //   .then((res) => {
           //     if (res.status == 200) {
-          //       return res.json(); //? 서버로부터 res로 받은 문구를 직접 alert에 띄우기 위한 코드
+          //       return res.json(); //! 서버로부터 res로 받은 문구를 직접 alert에 띄우기 위한 코드
           //     } else {
           //       //서버가 500, 400등 에러코드전송시 실행할코드
           //     }
@@ -28,26 +50,6 @@ export default function DeleteButton(props) {
           //     //인터넷문제 등으로 실패시 실행할코드
           //     console.log(error);
           //   });
-
-          //! URL parameter로 구현
-          fetch(`/api/post/delete/${props._id}`, { method: "GET" }) //props 자체가 {_id:xxx..}형태이기에
-            .then((res) => {
-              if (res.status == 200) {
-                return res.json(); //? 서버로부터 res로 받은 문구를 직접 alert에 띄우기 위한 코드
-              } else {
-                //서버가 500, 400등 에러코드전송시 실행할코드
-              }
-            })
-            .then((res) => {
-              alert(res);
-            })
-            .then(() => {
-              location.href = "/community";
-            })
-            .catch((error) => {
-              //인터넷문제 등으로 실패시 실행할코드
-              console.log(error);
-            });
         }}
         style={{ color: "red", fontSize: "small", marginTop: "4vh" }}
       >
