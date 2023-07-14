8728e3ed71a7fc193bccf626c1c1832ad44eee20
Jihyun <jhjh3903@gmail.com>
2023-07-15 01:39:51 +0900

    fix :type오류 옵셔널 체이닝 처리

--- app/community/edit/[id]/page.tsx
+++ app/community/edit/[id]/page.tsx
@@ -23,10 +23,10 @@ export default async function EditCommunityDetail(props: editContentProps) {
         <form action="/api/post/edit" method="POST">
           {/* 키이름은 name ='~' , 칸 설명은 placeholder='~' */}
           <div>오늘의 노력을 간략하게 소개해주세요.</div>
-          <input className="title_box" name="title" placeholder="제목" defaultValue={result.title} />
+          <input className="title_box" name="title" placeholder="제목" defaultValue={result?.title} />
           <div>어떻게 성장할까요?</div>
 
-          <textarea className="content_box" name="content" placeholder="내용" defaultValue={result.content} />
+          <textarea className="content_box" name="content" placeholder="내용" defaultValue={result?.content} />
           {/* <div> 연결된 노력이 있다면 선택해주세요  </div> */}
           <br />
           <div style={{ color: "red" }}>글 수정시 사진이 초기화되기에 반드시 재첨부해야합니다.</div>
@@ -34,7 +34,7 @@ export default async function EditCommunityDetail(props: editContentProps) {
           <input name="image" type="file" accept="image/jpg, image/jpeg, image/png" placeholder="사진을 첨부하세요" />
           {/* <input name="image" type="file" accept="image/jpg, image/jpeg, image/png" placeholder="사진을 첨부하세요" defaultValue={result.image} /> //! 디폴트밸류 설정해놓으면 오류로 안됨 일단 제거 */}
 
-          <input name="_id" style={{ display: "none" }} defaultValue={result._id.toString()} />
+          <input name="_id" style={{ display: "none" }} defaultValue={result?._id.toString()} />
           {/* //!아이디를 함께 보내야하기 때문에 input으로 id 넣어놓고 display: none 처리 */}
           <button type="submit">수정완료</button>
         </form>
